072acd374e106cf27a7f0b0b4618e8886d641299
Grégoire <gregoire.suray@gmail.com>
2024-02-29 21:04:38 +0100

    added model

---
+++ models/Thing.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+
+// La méthode Schema permet de créer un schéma de données pour BDD MongoDB.
+// La méthode model transforme ce modèle en un modèle utilisable.
+const thingSchema = mongoose.Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  imageUrl: { type: String, required: true },
+  userId: { type: String, required: true },
+  price: { type: Number, required: true },
+});
+
+module.exports = mongoose.model('Thing', thingSchema);
